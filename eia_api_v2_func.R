9e740bf6ae35fea72bf0e0632255a62019ce4112
Brett <34327248+brf217@users.noreply.github.com>
2023-07-14 09:09:25 -0600

    Create eia_api_v2_func.R

---
+++ eia_api_v2_func.R
@@ -0,0 +1,36 @@
+library(glue)
+library(jsonlite)
+library(httr)
+
+api_key = key
+
+eia_api = function(endpoint, series, api_key, freq){
+  # form query using GET from httr package
+  
+  if (freq == 'weekly'){
+    start_dt = '2017-01-01'}
+  else {
+    start_dt = '2017-01'}
+  
+  api_call = GET(glue(
+    'https://api.eia.gov/v2/{endpoint}?',
+    'api_key={api_key}',
+    '&data[0]=value',
+    '&facets[series][1]={series}',
+    '&frequency={freq}',
+    '&start={start_dt}'))
+  
+  # parse the response
+  response_list = parse_json(api_call, simplifyVector = T)
+  
+  # retrieve dataframe from the results
+  api_df = response_list$response$data
+  
+  # replace bad df column names with better ones
+  names(api_df) = gsub('[-&$?%]', '_', names(api_df))
+  
+  return(api_df)
+}
+
+ng_storage = eia_api('natural-gas/stor/wkly/data/', 'NW2_EPG0_SWO_R48_BCF',
+                   api_key,'weekly') 

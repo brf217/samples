21400f5a0ef889498b9fcb887d56d8af234b08d1
Brett <34327248+brf217@users.noreply.github.com>
2023-07-17 08:33:11 -0600

    Create account_utilization.sql

---
+++ account_utilization.sql
@@ -0,0 +1,36 @@
+---Date range of utilization and average monthly utilization by subscription
+
+WITH full_table AS(
+	SELECT  
+      facts.date_dimension_id, 
+      facts.subscription_dimension_id, 
+      subs.name,
+      subs.is_active,
+      subs.stack,
+      subs.paid_seats,
+  		facts.monthly_perc_user_utilization,
+      facts.weekly_perc_user_utilization,
+      facts.daily_users,
+      facts.health,facts.health2,
+      facts.monthly_artifact_growth,
+  		AVG(monthly_perc_user_utilization) OVER (PARTITION BY subscription_dimension_id) as avg_month_util_dtrange,
+  		AVG(weekly_perc_user_utilization) OVER (PARTITION BY subscription_dimension_id) as avg_week_util_dtrange,
+  		(facts.monthly_perc_user_utilization - AVG(monthly_perc_user_utilization) OVER (PARTITION BY subscription_dimension_id)) AS month_avg_dif,
+  		(facts.weekly_perc_user_utilization - AVG(weekly_perc_user_utilization) OVER (PARTITION BY subscription_dimension_id)) AS wkly_avg_dif
+	FROM 	
+      daily_health_metrics_facts AS facts
+	JOIN	
+      subscription_dimension AS subs ON subs.id = facts.subscription_dimension_id
+	WHERE 	
+      date_dimension_id BETWEEN '2018-04-30' AND '2018-06-01'
+  		AND is_active = 't'
+  		AND stack IN ('prod' , 'emea')
+  		AND subscription_dimension_id IN (SELECT subscription_dimension_id
+			                                  FROM daily_health_metrics_facts
+                                        WHERE date_dimension_id BETWEEN '2018-04-30' AND '2018-06-01'
+                                        GROUP BY subscription_dimension_id
+                                        HAVING COUNT(subscription_dimension_id) > 30)
+                                      	ORDER BY subscription_dimension_id, date_dimension_id DESC)
+SELECT DISTINCT ON (subscription_dimension_id) * 
+FROM full_table
+ORDER BY subscription_dimension_id, date_dimension_id DESC

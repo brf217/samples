0e3438804d568576a5f554a775811b3395cf2a91
Brett <34327248+brf217@users.noreply.github.com>
2024-08-15 15:12:00 -0600

    Create streamlit_chatbot.py

---
+++ streamlit_chatbot.py
@@ -0,0 +1,249 @@
+import streamlit as st
+from snowflake.core import Root # requires snowflake>=0.8.0
+from snowflake.snowpark.context import get_active_session
+import datetime as dt
+from snowflake.cortex import Complete
+
+# basic config
+today = dt.date.today()
+st.set_page_config(layout="wide")
+MODELS = [
+    "mistral-large"
+]
+
+def init_messages():
+    """
+    Initialize the session state for chat messages. If the session state indicates that the
+    conversation should be cleared or if the "messages" key is not in the session state,
+    initialize it as an empty list.
+    """
+    if st.session_state.clear_conversation or "messages" not in st.session_state:
+        st.session_state.messages = []
+
+def init_service_metadata():
+    """
+    Initialize the session state for cortex search service metadata. Query the available
+    cortex search services from the Snowflake session and store their names and search
+    columns in the session state.
+    """
+    if "service_metadata" not in st.session_state:
+        services = session.sql("SHOW CORTEX SEARCH SERVICES;").collect()
+        service_metadata = []
+        if services:
+            for s in services:
+                svc_name = s["name"]
+                svc_search_col = session.sql(
+                    f"DESC CORTEX SEARCH SERVICE {svc_name};"
+                ).collect()[0]["search_column"]
+                service_metadata.append(
+                    {"name": svc_name, "search_column": svc_search_col}
+                )
+
+        st.session_state.service_metadata = service_metadata
+
+        
+def init_config_options():
+    """
+    Initialize the configuration options in the Streamlit sidebar. Allow the user to select
+    a cortex search service, clear the conversation, toggle debug mode, and toggle the use of
+    chat history. Also provide advanced options to select a model, the number of context chunks,
+    and the number of chat messages to use in the chat history.
+    """
+
+    st.sidebar.button("Clear conversation", key="clear_conversation")
+    #st.sidebar.toggle("Debug", key="debug", value=False)
+    st.sidebar.toggle("Use chat history", key="use_chat_history", value=True)
+
+    with st.sidebar.expander("Advanced options"):
+        st.selectbox("Select model:", MODELS, key="model_name")
+        st.number_input(
+            "Select number of context chunks",
+            value=5,
+            key="num_retrieved_chunks",
+            min_value=1,
+            max_value=10,
+        )
+        st.number_input(
+            "Select number of messages to use in chat history",
+            value=5,
+            key="num_chat_messages",
+            min_value=1,
+            max_value=10,
+        )
+
+    st.sidebar.expander("Session State").write(st.session_state)
+
+
+def get_chat_history():
+    """
+    Retrieve the chat history from the session state limited to the number of messages specified
+    by the user in the sidebar options.
+
+    Returns:
+        list: The list of chat messages from the session state.
+    """
+    start_index = max(
+        0, len(st.session_state.messages) - st.session_state.num_chat_messages
+    )
+    return st.session_state.messages[start_index : len(st.session_state.messages) - 1]
+
+def complete(model, prompt):
+    """
+    Generate a completion for the given prompt using the specified model.
+
+    Args:
+        model (str): The name of the model to use for completion.
+        prompt (str): The prompt to generate a completion for.
+
+    Returns:
+        str: The generated completion.
+    """
+    return Complete(model, prompt)
+
+
+def make_chat_history_summary(chat_history, question):
+    """
+    Generate a summary of the chat history combined with the current question to extend the query
+    context. Use the language model to generate this summary.
+
+    Args:
+        chat_history (str): The chat history to include in the summary.
+        question (str): The current user question to extend with the chat history.
+
+    Returns:
+        str: The generated summary of the chat history and question.
+    """
+    prompt = f"""
+        [INST]
+        Based on the chat history below and the question, generate a query that extend the question
+        with the chat history provided. The query should be in natural language.
+        Answer with only the query. Do not add any explanation.
+
+        <chat_history>
+        {chat_history}
+        </chat_history>
+        <question>
+        {question}
+        </question>
+        [/INST]
+    """
+
+    summary = complete(st.session_state.model_name, prompt)
+
+    return summary
+
+def create_prompt(user_question):
+    """
+    Create a prompt for the language model by combining the user question with context retrieved
+    from the cortex search service and chat history (if enabled). Format the prompt according to
+    the expected input format of the model.
+
+    Args:
+        user_question (str): The user's question to generate a prompt for.
+
+    Returns:
+        str: The generated prompt for the language model.
+    """
+    if st.session_state.use_chat_history:
+        chat_history = get_chat_history()
+        if chat_history != []:
+            question_summary = make_chat_history_summary(chat_history, user_question)
+            prompt_context = question_summary
+        else:
+            prompt_context = user_question
+    else:
+        prompt_context = user_question
+        chat_history = ""
+
+    prompt = f"""
+            [INST]
+            Acting as a financial analyst reading a deal announcement,
+            please provide a summary of the message as a table with column headers representing
+            the following:
+            Launch Date in mm/dd/yyy format - if none given use {today},
+            Amount(in $mm) without the '$' symbol,
+            Issuer as text, 
+            Bloomberg FIGI as text - do not fill if it is not found,
+            Issuance Type as text,
+            Business as text, representing the issuer's industry,
+            Price Talk or IPT as shown, 
+            Call Date and Time mm/dd hh:mm format converted to MST timezone and 
+            saved as a text field - this is not the same as callable or non-callable,
+            Commit Date in mm/dd hh:mm format converted to MST timezone,
+            Use of Procceds(UoP) in 5 words or less - this is what the money will be used for,
+            Tenor converted to number of years to one decimal and include YR suffix,
+            Redemption Feature as text,
+            Ratings as text,
+            First Lead underwriter as text.
+            
+            We want the return to be in table format with 
+            blank in the column if one of the values cannot be found. 
+            Redemption features for bonds generally 
+            start with NC and end with a number.
+            Redemption features for loans generally mention soft call.
+            You can use the text and web to determine business industry and 
+            Bloomberg FIGI. When selecting a Bloomberg FIGI choose the one 
+            with a maturity date that would be appropriate given tenor
+            
+            Please use the following to fill out the table above. If any of the information
+            is not available, please just leave blank and do not guess. All answers should be
+            in this same table format.
+
+            <chat_history>
+            {chat_history}
+            </chat_history>
+            <context>
+            {prompt_context}
+            </context>
+            <question>
+            {user_question}
+            </question>
+            [/INST]
+            Answer:
+        """
+    return prompt
+
+    
+def main():
+    st.title(f":speech_balloon: SF Cortex New Issue Conversation")
+
+    init_service_metadata()
+    init_config_options()
+    init_messages()
+
+    icons = {"assistant": "❄️", "user": "👤"}
+
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"], avatar=icons[message["role"]]):
+            st.markdown(message["content"])
+
+    disable_chat = (
+        "service_metadata" not in st.session_state
+        or len(st.session_state.service_metadata) == 0
+    )
+    if question := st.chat_input("Paste deal announcement to create table..."):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": question})
+        # Display user message in chat message container
+        with st.chat_message("user", avatar=icons["user"]):
+            st.markdown(question.replace("$", "\$"))
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant", avatar=icons["assistant"]):
+            message_placeholder = st.empty()
+            question = question.replace("'", "")
+            with st.spinner("Thinking..."):
+                generated_response = complete(
+                    st.session_state.model_name, create_prompt(question)
+                )
+                message_placeholder.markdown(generated_response)
+
+        st.session_state.messages.append(
+            {"role": "assistant", "content": generated_response}
+        )
+
+if __name__ == "__main__":
+    session = get_active_session()
+    root = Root(session)
+    main()
